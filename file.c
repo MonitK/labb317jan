52c431e9b3f48df7f2de07cbeca49414724110ef
MonitK <mkapoor@ddn.upes.ac.in>
2019-01-17 10:26:59 +0530

    added class, second commit

--- file.c
+++ file.c
@@ -1,4 +1,21 @@
-#include<iostream.h>
+#include<iostream.h
+class CALC
+{
+	void substract(int,int);
+	int multiply(int,int)
+	void add(int,int);
+	int divide(int,int);
+	public:
+	int x,y,choice;
+};
+int main()
 {
 cout<<"hllo world"<<endl;
 }
+
+
+
+
+
+
+
